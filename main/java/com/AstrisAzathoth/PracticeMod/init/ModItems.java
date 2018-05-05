c02a081fa4f9a48c635f96e724db1c54535f286a
AstrisAzathoth <39001670+AstrisAzathoth@users.noreply.github.com>
2018-05-05 18:50:43 +0800

    Add files via upload

--- main/java/com/AstrisAzathoth/PracticeMod/init/ModItems.java
+++ main/java/com/AstrisAzathoth/PracticeMod/init/ModItems.java
@@ -26,7 +26,7 @@ public class ModItems
 	
 	//Materials
 	public static final ToolMaterial TOOL_SHALE = EnumHelper.addToolMaterial("tool_shale", 2, 500, 6.0F, 2.0F, 22);
-	public static final ArmorMaterial ARMOR_SHALE = EnumHelper.addArmorMaterial("armor_shale", Reference.MOD_ID + "shale", 30, new int[]{2, 5, 6, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+	public static final ArmorMaterial ARMOR_SHALE = EnumHelper.addArmorMaterial("armor_shale", Reference.MOD_ID + ":shale", 30, new int[]{2, 5, 6, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
 	
 	//Items
 	public static final Item SHALE = new ItemBase("shale");
